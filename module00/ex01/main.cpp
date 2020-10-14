d89a61b050dab7284210e9174d7021fb89ce31d4
lejulien-42 <lejulien@student.42.fr>
2020-10-14 02:06:22 +0200

    completing ex01

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -5,9 +5,27 @@
  */
 
 #include "contact.hpp"
+#include <string>
+#include <iostream>
 
 int
 	main()
 {
+	int		con_set = 0;
 	Contact	annuaire[8];
+	bool	is_exit = false;
+	int		menu = 0;
+
+	while (!is_exit)
+	{
+		std::string	typed;
+		getline(std::cin, typed);
+
+		if (menu == 0)
+		{
+			if (typed.compare("EXIT"))
+				std::cout << "ok" << std::endl;
+		}
+	}
+	return (0);
 }
