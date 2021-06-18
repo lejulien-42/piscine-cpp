a64da1cf4a5ebbce813c5b7eebf41d71b7669a2d
lejulien-42 <lejulien@student.42.fr>
2021-06-18 05:09:01 +0200

    starting ex02

---
+++ module04/ex02/ISquad.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ISquad.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lejulien <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/18 05:07:08 by lejulien          #+#    #+#             */
+/*   Updated: 2021/06/18 05:08:05 by lejulien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class ISquad
+{
+public:
+	virtual ~ISquad() {}
+	virtual int getCount() const = 0;
+	virtual ISpaceMarine* getUnit(int) const = 0;
+	virtual int push(ISpaceMarine*) = 0;
+};
