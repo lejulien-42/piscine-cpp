4e609befcd11acb84a5c07834b290907da485670
lejulien-42 <lejulien@student.42.fr>
2021-07-16 03:13:45 +0200

    testing

---
+++ module08/ex02/MutantStack.cpp
@@ -0,0 +1,12 @@
+//
+// -Created by lejulien @ 42-
+//
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include "MutantStack.hpp"
+
+// Empty because the class is an entire templated class
+
+// ----- by lejulien -----
